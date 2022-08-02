31fb3a90fb6c4bcb265dd34e61eb4518ea547492
kkookkss <pre0919@gmail.com>
2022-08-03 06:08:55 +0900

    @RequestParam과 @ModelAttribute

--- ch2/src/main/java/com/spring/ch2/PerformanceFilter.java
+++ ch2/src/main/java/com/spring/ch2/PerformanceFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
 // 필터를 적용할 요청의 패턴 지정 - 모든 요청에 필터를 적용.
-@WebFilter(urlPatterns="/*") // @WebFilter: Filter등록시 사용하는 어노테이션
+@WebFilter(urlPatterns="/*") // @WebFilter: Filter등록시 사용하는 어노테이
 public class PerformanceFilter implements Filter {
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
